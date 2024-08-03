d17cfcb9fd1d0ce7689c8cb4babe0fac4966cea7
GUAN MING <guanmingchiu@gmail.com>
2024-08-04 01:17:37 +0800

    style: fix tab color

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,10 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from "expo-router";
+import React from "react";
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { useThemeColor } from "@/hooks/useThemeColor";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -11,24 +12,41 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerShown: false,
-      }}>
+        tabBarStyle: {
+          backgroundColor: useThemeColor(
+            {
+              light: Colors["light"].background,
+              dark: Colors["dark"].background,
+            },
+            "background"
+          ),
+          borderTopWidth: 0,
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: "Home",
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+            <TabBarIcon
+              name={focused ? "home" : "home-outline"}
+              color={color}
+            />
           ),
         }}
       />
       <Tabs.Screen
         name="outfit"
         options={{
-          title: 'Outfit',
+          title: "Outfit",
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'shirt' : 'shirt-outline'} color={color} />
+            <TabBarIcon
+              name={focused ? "shirt" : "shirt-outline"}
+              color={color}
+            />
           ),
         }}
       />
